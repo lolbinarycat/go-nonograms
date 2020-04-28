9b4916f59a8e867c309cba90a1e45d6a69f5560c
binarycat <dogedoge61@gmail.com>
2020-04-28 12:54:08 -0400

    cursor can be moved and cell states can be changed

--- main.go
+++ main.go
@@ -2,29 +2,27 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"errors"
-	"os"
+	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/gdamore/tcell"
 	"strconv"
 )
 
 type NonogramData struct {
-	Name string
+	Name          string
 	Width, Height int32
 	Columns, Rows [][]int
 }
 
-
-
 func (data *NonogramData) CheckData() error {
 	switch {
-	case int32(len(data.Columns)) != data.Width :
-		return errors.New(fmt.Sprintf("json error: number of columns (%d) is not equal to width (%d)",len(data.Columns),data.Width))
-	case int32(len(data.Rows)) != data.Height :
-		return errors.New(fmt.Sprintf("json error: number of rows (%d) is not equal to height (%d)",len(data.Rows),data.Height))
+	case int32(len(data.Columns)) != data.Width:
+		return errors.New(fmt.Sprintf("json error: number of columns (%d) is not equal to width (%d)", len(data.Columns), data.Width))
+	case int32(len(data.Rows)) != data.Height:
+		return errors.New(fmt.Sprintf("json error: number of rows (%d) is not equal to height (%d)", len(data.Rows), data.Height))
 	}
 
 	return nil
@@ -35,16 +33,29 @@ func MakeBoardState(data NonogramData) NonogramBoardState {
 
 	var nbs NonogramBoardState
 
-	nbs = make([][]NonogramCellState,data.Width + 1)
+	nbs = make([][]NonogramCellState, data.Width+1)
 
 	for i := range nbs {
-		nbs[i] = make([]NonogramCellState,data.Height + 1)
+		nbs[i] = make([]NonogramCellState, data.Height+1)
 		i--
 	}
 	return nbs
 }
 
 type NonogramBoardState [][]NonogramCellState
+
+func (cell *NonogramCellState) Cycle() {
+	defer func() {
+		if recover() != nil {
+			*cell = 0
+		}
+	}()
+	if *cell + 2 == EndState {
+		*cell = 0
+	} else {
+		*cell++
+	}
+}
 	
 
 type NonogramCellState int
@@ -52,30 +63,42 @@ type NonogramCellState int
 const (
 	Empty NonogramCellState = iota + 1
 	Filled
+	EndState //this is here for logic reasons, it is not an actual state
 )
 
+type Direction int
+
+const (
+	Up Direction = iota + 1
+	Down
+	Left
+	Right
+)
 
 var style = tcell.StyleDefault
 
-var stateRunes = [2]rune{ '0','\u2588' } //describes how to display board
+var stateRunes = [...]rune{'0', '\u2588','e'} //describes how to display board
+
+var (
+	cursorRune = '\u25A3'
+	cursorPos  = [2]int{0, 0}
+)
 
 func main() {
 
-	
-	
 	nonogramFile, err := os.Open("test.json")
 	ec(err)
 	nonogramBytes, err := ioutil.ReadAll(nonogramFile)
 	ec(err)
 
 	var nonogramData NonogramData
-	
-	ec(json.Unmarshal(nonogramBytes,&nonogramData))
+
+	ec(json.Unmarshal(nonogramBytes, &nonogramData))
 	ec(nonogramData.CheckData())
 
 	//fmt.Println(nonogramData)
 	//os.Exit(53)
-	
+
 	tcell.SetEncodingFallback(tcell.EncodingFallbackASCII)
 	screen, err := tcell.NewScreen()
 	ec(err)
@@ -90,11 +113,17 @@ func main() {
 	screen.Clear()
 
 	//fmt.Println("\a")
-	
-	var nonogramBoardState NonogramBoardState = MakeBoardState(nonogramData) //make([][]NonogramCellState,10)
+
+	var nonState NonogramBoardState = MakeBoardState(nonogramData) //make([][]NonogramCellState,10)
+
+	upScrn := func() { //update the screen
+		drawNonogram(nonogramData, nonState, screen)
+		screen.Show()
+	}
 
 	quit := make(chan struct{})
 	go func() {
+		defer screen.Fini()
 		for {
 			ev := screen.PollEvent()
 			switch ev := ev.(type) {
@@ -105,25 +134,43 @@ func main() {
 					return
 				case tcell.KeyCtrlL:
 					screen.Sync()
+				case tcell.KeyUp:
+					moveCursor(Up, 1, nonState)
+					upScrn()
+				case tcell.KeyDown:
+					moveCursor(Down, 1, nonState)
+					drawNonogram(nonogramData, nonState, screen)
+					screen.Show()
+				case tcell.KeyLeft:
+					moveCursor(Left, 1, nonState)
+					drawNonogram(nonogramData, nonState, screen)
+					screen.Show()
+				case tcell.KeyRight:
+					moveCursor(Right, 1, nonState)
+					drawNonogram(nonogramData, nonState, screen)
+					screen.Show()
+					
 				default:
-					switch ev.Rune(){
+					switch ev.Rune() {
 					case 'a':
-						screen.SetCell(1,1,tcell.StyleDefault,'0')
+						screen.SetCell(1, 1, tcell.StyleDefault, '0')
 						screen.Show()
+					case ' ':
+						nonState[cursorPos[0]+1][cursorPos[1]+1].Cycle()
+						//upScrn()
 					case 'q':
 						close(quit)
 						return
 					}
 				}
-				
-				
+
 			case *tcell.EventResize:
 				screen.Sync()
 			}
 		}
 	}()
-	
-	drawNonogram(nonogramData,nonogramBoardState,screen)	
+
+	drawNonogram(nonogramData, nonState, screen)
 
 	screen.Show()
 
@@ -135,8 +182,6 @@ loop:
 		}
 	}
 
-	
-	
 }
 
 func ec(err error) {
@@ -145,59 +190,81 @@ func ec(err error) {
 		os.Exit(1)
 	}
 }
-	
 
-
-func drawNonogram(non NonogramData,nonState NonogramBoardState,screen tcell.Screen) {
+func drawNonogram(non NonogramData, nonState NonogramBoardState, screen tcell.Screen) {
 
 	defer screen.Show()
 
-	
 	i := int(non.Width)
 
 	offset := 5
-	
+
 	for i > 0 {
 		j := int(non.Height)
 		for j > 0 {
 
-			runeToDraw := 'e'
-			
+			runeToDraw := 'e' //if this shows up, something has gone wrong.
+
 			if len(nonState) >= i && len(nonState[i]) >= j {
 				runeToDraw = stateRunes[int(nonState[i][j])]
 			} else {
-				panic("error: nonogramBoardState is too short")
+				panic("error: nonState is too short")
 			}
-			
-			screen.SetCell(i+offset,j+offset,tcell.StyleDefault,runeToDraw)
+
+			screen.SetCell(i+offset, j+offset, tcell.StyleDefault, runeToDraw)
 			j--
 		}
 		i--
 	}
 
 	for i, _ := range non.Rows {
-		for j , hint := range non.Rows[i] {
+		for j, hint := range non.Rows[i] {
 			hintStr := strconv.Itoa(hint)
-			screen.SetCell(offset+j-len(non.Rows[i]),offset+i+1,style,rune(hintStr[0]))
+			screen.SetCell(offset+j-len(non.Rows[i]), offset+i+1, style, rune(hintStr[0]))
 		}
 	}
-
-	
 
 	for i, _ := range non.Columns {
-		for j , hint := range non.Columns[i] {
+		for j, hint := range non.Columns[i] {
 			hintStr := strconv.Itoa(hint)
-			screen.SetCell(offset+i+1,offset+j-len(non.Columns[i]),style,rune(hintStr[0]))
+			screen.SetCell(offset+i+1, offset+j-len(non.Columns[i]), style, rune(hintStr[0]))
 		}
 	}
 
+	screen.SetCell(cursorPos[0]+offset+1,cursorPos[1]+offset+1, style, cursorRune)
+		
 
 	screen.Show()
 }
-		
+
+func moveCursor(dir Direction, dist int, nonState NonogramBoardState) (moveCompleted bool) {
+	cursorPosBuffer := cursorPos
+	cursorMax := [2]int{len(nonState), len(nonState[0])}
+
+	switch dir {
+	case Up:
+		cursorPosBuffer[1] -= dist //because origin is at top left, going up involves decreacing the y value
+	case Down:
+		cursorPosBuffer[1] += dist
+	case Left:
+		cursorPosBuffer[0] -= dist
+	case Right:
+		cursorPosBuffer[0] += dist
+	default:
+		panic(fmt.Sprintf("moveCursor: %d is not recognized as a direction", dir))
+	}
+	if cursorPosBuffer[0] <= cursorMax[0] && cursorPosBuffer[0] >= 0 && cursorPosBuffer[1] <= cursorMax[1] && cursorPosBuffer[1] >= 0 { //this massive statement checks if the cursor would be in a valid position
+		cursorPos = cursorPosBuffer
+		return true
+	} else {
+		return false
+	}
+
+}
+
 /* func tryGetFromSlice(slice []interface{}, indexes ...int) (retrivedValue {
-	defer func() {
+defer func() {
 
-	}()
+}()
 
-	slice[indexes[0]]  */
+slice[indexes[0]]  */
